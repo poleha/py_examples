f1635c2c08d881f9539a3144b3d29d24e5c87c56
poleha <pass1191>
2016-07-02 10:10:13 +0300

    new tasks

--- try.py
+++ try.py
@@ -1,34 +1,46 @@
-def get_primes(N):
-    primes = []
-    visited = set()
-    i = 1
-    while i < N:
-        i += 1
-        if i in visited:
-            continue
-        j = i
-        first = True
-        primes.append(i)
-        while j <= N:
-            visited.add(j)
+from math import ceil
 
-            if first:
-                j *= j
-                first = False
-            else:
-                j += i
-    return primes
+A = [1, 2, 3, 4, 5]
+
+def get_prefixed_sums(A, L):
+    sums = {}
+    s = 0
+    for i in range(L):
+        cur = A[i]
+        s += cur
+        sums[i] = s
+
+    return sums
+
+sums = get_prefixed_sums(A, len(A))
+
+L = len(A)
+
+mid = sum(A) / L
+
+def bin_search(A, sums, start, mx, s=0, pos=0):
+    if pos == 0:
+        pos = int(ceil(mx / mid))
+    end = min(start + pos, L - 1)
+    sm = sums[end] - sums[start] + A[start]
+    if sm == mx:
+        return sm, end
+    elif sm < mx:
+        delta = int(ceil(mx - s) / mid)
+        pos += delta
+        res = bin_search(A, sums, start, mx, s, pos)
+    else:
+        if sm - A[end] < mx:
+            return sm - A[end], pos -1
+        delta = int(ceil(mx - s) / mid)
+        pos -= delta
+        res = bin_search(A, sums, start, mx, s, pos)
+    return res
 
 
+res = bin_search(A, sums, 0, 4)
 
-
-
-
-
-
-
-for j in get_primes(25):
-    print(j)
+print(res)
 
 
 
