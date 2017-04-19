86f920ad3e9604d2f9a61be3054e4648db217e06
Alex Polekha <a.polekha@gmail.com>
2017-04-19 14:49:46 +0300

    Interesting task added to brain training

--- kulik.py
+++ kulik.py
@@ -1640,3 +1640,79 @@ loop.schedule(task())
 loop.schedule(simple_task())
 
 loop.run()
+
+
+#**************************************
+
+"""
+https://codility.com/programmers/task/min_abs_sum/
+For a given array A of N integers and a sequence S of N integers from the set {−1, 1}, we define val(A, S) as follows:
+
+val(A, S) = |sum{ A[i]*S[i] for i = 0..N−1 }|
+(Assume that the sum of zero elements equals zero.)
+
+For a given array A, we are looking for such a sequence S that minimizes val(A,S).
+
+Write a function:
+
+def solution(A)
+that, given an array A of N integers, computes the minimum value of val(A,S) from all possible values of val(A,S) for all possible sequences S of N integers from the set {−1, 1}.
+
+For example, given array:
+
+  A[0] =  1
+  A[1] =  5
+  A[2] =  2
+  A[3] = -2
+your function should return 0, since for S = [−1, 1, −1, 1], val(A, S) = 0, which is the minimum possible value.
+
+Assume that:
+
+N is an integer within the range [0..20,000];
+each element of array A is an integer within the range [−100..100].
+Complexity:
+
+expected worst-case time complexity is O(N*max(abs(A))2);
+expected worst-case space complexity is O(N+sum(abs(A))), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+"""
+
+
+# stud
+# Obvioulsly slow, but I love recursions
+def step(A, L, i, cur):
+    cur1 = cur + A[i]
+    cur2 = cur - A[i]
+    if i < L - 1:
+        var1 = step(A, L, i + 1, cur1)
+        var2 = step(A, L, i + 1, cur2)
+        return min(var1, var2)
+    else:
+        return min(abs(cur1), abs(cur2))
+
+
+def solution1(A):
+    if not A:
+        return 0
+    L = len(A)
+    var = step(A, L, 0, 0)
+    return var
+
+#***
+
+# stud
+def step4(A, n):
+    if n == 0:
+        return {A[0]}
+    else:
+        res = set()
+        for a in step4(A, n - 1):
+            res.add(abs(a - A[n]))
+            res.add(abs(a + A[n]))
+        return res
+
+def solution4(A):
+    if not A:
+        return 0
+    L = len(A)
+    return min(step4(A, L - 1))
